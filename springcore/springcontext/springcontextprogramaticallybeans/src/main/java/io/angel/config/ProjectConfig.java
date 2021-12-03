bd1056eab624a7be399a9f5e571a83200cb8f33e
Angel Gruevski <angelgruevski@protonmail.com>
2021-12-03 15:38:00 +0100

    Add springcontextprogramaticallybeans

--- springcore/springcontext/springcontextprogramaticallybeans/src/main/java/io/angel/config/ProjectConfig.java
+++ springcore/springcontext/springcontextprogramaticallybeans/src/main/java/io/angel/config/ProjectConfig.java
@@ -1,10 +1,7 @@
 package io.angel.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"io.angel.main"})
 public class ProjectConfig {
-    
 }
---
+++ springcore/springcontext/springcontextprogramaticallybeans/src/main/java/io/angel/main/Main.java
@@ -0,0 +1,22 @@
+package io.angel.main;
+
+import java.util.function.Supplier;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import io.angel.config.ProjectConfig;
+
+public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Parrot p = new Parrot();
+        p.setName("Kiki");
+
+        Supplier<Parrot> parrotSupplier = () -> p;
+
+        context.registerBean("parrot1", Parrot.class, parrotSupplier]);
+
+        Parrot parrot = context.getBean(Parrot.class);
+    }
+}

4beb02ea73daf301f16389c13404d70c7db52191
Angel Gruevski <angelgruevski@protonmail.com>
2021-12-06 12:07:00 +0100

    Add BeansRelationship

---
+++ springcore/springcontext/BeansRelationship/src/main/java/io/angel/main/Main.java
@@ -0,0 +1,14 @@
+package io.angel.main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import io.angel.config.ProjectConfig;
+
+public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Person p = context.getBean(Person.class);
+        System.out.println(p.getParrot());
+    }
+}

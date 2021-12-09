286c5a2bcec188fc063b7df8e515f33155d6face
Angel Gruevski <angelgruevski@protonmail.com>
2021-12-09 14:14:05 +0100

    choosingfrommultiplebeansusingqualifierannotationandDI/

---
+++ springcore/springcontext/choosingfrommultiplebeansusingqualifierannotationandDI/src/main/java/io/angel/config/ProjectConfig.java
@@ -0,0 +1,27 @@
+package io.angel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import io.angel.beans.Parrot;
+import io.angel.beans.Person;
+
+@Configuration
+@ComponentScan(basePackages = {"io.angel.beans"})
+public class ProjectConfig {
+    
+    @Bean
+    public Parrot parrot1() {
+        Parrot parrot = new Parrot();
+        parrot.setName("Kiki");
+        return parrot;
+    }
+
+    @Bean
+    public Parrot parrot2() {
+        Parrot parrot = new Parrot();
+        parrot.setName("Miki");
+        return parrot;
+    }
+}

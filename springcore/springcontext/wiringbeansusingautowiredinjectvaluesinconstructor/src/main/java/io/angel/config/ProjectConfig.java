a125ed18942ee8044fb44b040371b1c420175335
Angel Gruevski <angelgruevski@protonmail.com>
2021-12-09 08:07:37 +0100

    Add wiringbeansusingautowiredinjectingvalueinfieldofclass

---
+++ springcore/springcontext/wiringbeansusingautowiredinjectvaluesinconstructor/src/main/java/io/angel/config/ProjectConfig.java
@@ -0,0 +1,10 @@
+package io.angel.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "io.angel.beans")
+public class ProjectConfig {
+
+}

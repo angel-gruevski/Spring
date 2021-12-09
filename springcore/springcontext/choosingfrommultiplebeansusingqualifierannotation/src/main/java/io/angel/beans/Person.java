b0a8358009a1701e14cc3aef823f86e62e85dc51
Angel Gruevski <angelgruevski@protonmail.com>
2021-12-09 11:50:20 +0100

    Add choosingfrommultiplebeansusingqualifierannotation/ and choosingfrommultiplebeans/

---
+++ springcore/springcontext/choosingfrommultiplebeansusingqualifierannotation/src/main/java/io/angel/beans/Person.java
@@ -0,0 +1,23 @@
+package io.angel.beans;
+
+public class Person {
+
+  private String name = "Ella";
+  private Parrot parrot;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Parrot getParrot() {
+    return parrot;
+  }
+
+  public void setParrot(Parrot parrot) {
+    this.parrot = parrot;
+  }
+}
---
+++ springcore/springcontext/choosingfrommultiplebeansusingqualifierannotation/src/main/java/io/angel/config/ProjectConfig.java
@@ -0,0 +1,33 @@
+package io.angel.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.angel.beans.Parrot;
+import io.angel.beans.Person;
+
+@Configuration
+public class ProjectConfig {
+    @Bean
+    public Parrot parrot1() {
+        Parrot p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
+
+    @Bean
+    public Parrot parrot2() {
+        Parrot p = new Parrot();
+        p.setName("Miki");
+        return p;
+    }
+
+    @Bean
+    public Person person(@Qualifier("parrot2") Parrot parrot) {
+        Person p = new Person();
+        p.setName("Ella");
+        p.setParrot(parrot);
+        return p;
+    } 
+}
